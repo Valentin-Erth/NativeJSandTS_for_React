fc0bf9d0df7ea5aee8d88dc40468442d1754e09f
Kozlov Valentin <kozlov0020@gmail.com>
2023-03-13 22:03:44 +0500

    add full copy for array with use methods crud(add,remove,change), test

--- src/10/10_01.test.ts
+++ src/10/10_01.test.ts
@@ -1,12 +1,14 @@
-import {moveUser, upgradeUserLaptop} from "./10_01";
+import {
+    addNewBooksToUser,
+    moveUser,
+    moveUserToOtherHouse, removeBook, updateBook, updateCompanyTitle,
+    upgradeUserLaptop,
+    UserWitthBooksType,
+    UserWitthLaptopType, WitthCompaniesType
+} from "./10_01";
 
-export type UserWitthLaptopType = {
-    name: string
-    hair: number
-    address: { city: string, house?: number }
-    laptop: { title: string }
-}
-test("cnahge adress", () => {
+
+test("cnahge laptop", () => {
     let user: UserWitthLaptopType = {
         name: "Dimych",
         hair: 32,
@@ -18,14 +20,120 @@ test("cnahge adress", () => {
             title: "ZenBook"
         }
     }
-
     // const movedUser=moveUser(user, "Kiev")
     // expect(user.address).not.toBe(movedUser.address)
     // expect(user.laptop).toBe(movedUser.laptop)
     // expect(movedUser.adress.city).toBe("Kiev")
-    const userCopy=upgradeUserLaptop(user, "Macbook")
+    const userCopy = upgradeUserLaptop(user, "Macbook")
     expect(user.address).toBe(userCopy.address)
     expect(user.laptop).not.toBe(userCopy.laptop)
     expect(userCopy.laptop.title).toBe("Macbook")
     expect(user.laptop.title).toBe("ZenBook")
+})
+test("cnahge adress house", () => {
+    let user: UserWitthLaptopType & UserWitthBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "ZenBook"
+        },
+        books: ["css", "html", "js", "react"]
+    }
+    // const movedUser=moveUser(user, "Kiev")
+    // expect(user.address).not.toBe(movedUser.address)
+    // expect(user.laptop).toBe(movedUser.laptop)
+    // expect(movedUser.adress.city).toBe("Kiev")
+    const userCopy = moveUserToOtherHouse(user, 100)
+    expect(user.books).toBe(userCopy.books)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).not.toBe(userCopy.address)
+    expect(user.address.house).toBe(100)
+})
+test("add new books to user", () => {
+    let user: UserWitthLaptopType & UserWitthBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "ZenBook"
+        },
+        books: ["css", "html", "js", "react"]
+    }
+       const userCopy = addNewBooksToUser(user, ["ts", "rest api"])
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[4]).toBe("ts")
+
+})
+test("update js to ts", () => {
+    let user: UserWitthLaptopType & UserWitthBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "ZenBook"
+        },
+        books: ["css", "html", "js", "react"]
+    }
+    const userCopy = updateBook(user, "js", "ts")
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[2]).toBe("ts")
+    expect(user.books.length).toBe(4)
+
+})
+test("remove js", () => {
+    let user: UserWitthLaptopType & UserWitthBooksType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "ZenBook"
+        },
+        books: ["css", "html", "js", "react"]
+    }
+    const userCopy = removeBook(user, "js")
+    expect(user).not.toBe(userCopy)
+    expect(user.laptop).toBe(userCopy.laptop)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.books).not.toBe(userCopy.books)
+    expect(userCopy.books[2]).toBe("react")
+    expect(userCopy.books.length).toBe(3)
+
+})
+test("update company", () => {
+    let user: UserWitthLaptopType & WitthCompaniesType = {
+        name: "Dimych",
+        hair: 32,
+        address: {
+            city: "Minsk",
+            house: 12
+        },
+        laptop: {
+            title: "ZenBook"
+        },
+        companies: [{id:1,title:"Епам"},{id:2,title:"IT-Incubator"}]
+    }
+    const userCopy = updateCompanyTitle(user, 1, "EPAM")
+    expect(user).not.toBe(userCopy)
+    expect(user.address).toBe(userCopy.address)
+    expect(user.companies).not.toBe(userCopy.companies)
+    expect(userCopy.companies[0].title).toBe("EPAM")
 })
--- src/10/10_01.ts
+++ src/10/10_01.ts
@@ -1,11 +1,60 @@
-import {UserWitthLaptopType} from "./10_01.test";
-
-export function moveUser(u:UserWitthLaptopType,city:string){
-return{
-    ...u,adress:{
-        ...u.address,city:city
+export type UserType = {
+    name: string
+    hair: number
+    address: { city: string, house?: number }
+}
+export type UserWitthLaptopType = UserType & {
+    laptop: { title: string }
+}
+export type UserWitthBooksType = UserType & {
+    books: Array<string>
+}
+export type WitthCompaniesType = UserType & {
+    companies: Array<{id:number,title: string}>
+}
+type CompanyType={
+    id:number
+    title:string
+}
+export function moveUser(u: UserWitthLaptopType, city: string) {
+    return {
+        ...u, adress: {
+            ...u.address, city: city
+        }
     }
 }
+
+export function moveUserToOtherHouse(u: UserWitthLaptopType & UserWitthBooksType, house: number) {
+    return {
+        ...u, adress: {
+            ...u.address, house: house
+        }
+    }
 }
-export const upgradeUserLaptop =(u:UserWitthLaptopType,titleLaptop:string)=> ({...u,
-    laptop: {...u.laptop, title:titleLaptop}})
+
+export const upgradeUserLaptop = (u: UserWitthLaptopType, titleLaptop: string) => ({
+    ...u,
+    laptop: {...u.laptop, title: titleLaptop}
+})
+
+export function addNewBooksToUser(u: UserWitthLaptopType & UserWitthBooksType, newBooks: Array<string>) {
+    return {
+        ...u, books: [...u.books, ...newBooks]
+    }
+}
+
+export const updateBook = (u: UserWitthLaptopType & UserWitthBooksType, lastBook: string, newBook: string) => (
+        {...u,
+            books: u.books.map(b => b === lastBook ? newBook : b)}
+    )
+
+export const removeBook = (u: UserWitthLaptopType & UserWitthBooksType, Book: string) => (
+    {...u,
+        books: u.books.filter(b => b!== Book )}
+)
+export const updateCompanyTitle = (u:  WitthCompaniesType, companyId: number, newTitle: string) => (
+    {...u,
+        companies: u.companies.map(c => c.id === companyId ?{...c,title: newTitle } : c)
+    }
+)
+
